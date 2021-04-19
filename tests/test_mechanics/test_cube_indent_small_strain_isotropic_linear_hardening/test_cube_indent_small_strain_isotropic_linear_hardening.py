f87fa27614fbea42fb5054b1ec6888abb09adc10
Soda Mariane Cisse <sodamarianecisse@MacBook-Air-de-Soda.local>
2021-04-19 19:00:39 +0200

    .

--- tests/test_mechanics/test_cube_indent_small_strain_isotropic_linear_hardening/test_cube_indent_small_strain_isotropic_linear_hardening.py
+++ tests/test_mechanics/test_cube_indent_small_strain_isotropic_linear_hardening/test_cube_indent_small_strain_isotropic_linear_hardening.py
@@ -16,8 +16,8 @@ from h2o.problem.resolution.exact import solve_newton_exact
 class TestMecha(TestCase):
     def test_cube_indent_small_strain_isotropic_linear_hardening(self):
         # --- VALUES
-        P_min = 10.
-        P_max = 5.e6 / (16.e-3)
+        P_min = 0.
+        P_max = 350e6
         time_steps = np.linspace(P_min, P_max, 25)
         print(time_steps)
         iterations = 10
@@ -44,10 +44,10 @@ class TestMecha(TestCase):
 
         # --- MESH
         # mesh_file_path = "meshes/cube_indent.msh"
-        # mesh_file_path = "meshes/maillage_grossier.msh"
-        mesh_file_path = "meshes/cube_test.msh"
+        mesh_file_path = "meshes/maillage_grossier.msh"
+        #mesh_file_path = "meshes/cube_test.msh"
         # mesh_file_path = "meshes/try_0.msh"
-
+        #mesh_file_path = "meshes/cube_maillage_triangulaire_grossier.msh"
         # --- FIELD
         displacement = Field(label="U", field_type=FieldType.DISPLACEMENT_LARGE_STRAIN)
 
@@ -76,11 +76,11 @@ class TestMecha(TestCase):
         # --- MATERIAL
         parameters = {"YoungModulus": 206.e9, "PoissonRatio": 0.29, "HardeningSlope": 10.0e9, "YieldStress": 300.0e6}
         # stabilization_parameter = 1000. * parameters["YoungModulus"] / (1.0 + parameters["PoissonRatio"])
-        stabilization_parameter = 0.0001 * parameters["YoungModulus"] / (1.0 + parameters["PoissonRatio"])
+        stabilization_parameter = 0.01 * parameters["YoungModulus"] / (1.0 + parameters["PoissonRatio"])
         # stabilization_parameter = 1.0 * parameters["YoungModulus"] / (1.0 + parameters["PoissonRatio"])
         mat = Material(
             nq=p.mesh.number_of_cell_quadrature_points_in_mesh,
-            library_path="behaviour/src/libBehaviour.so",
+            library_path="behaviour/src/libBehaviour.dylib",
             library_name="FiniteStrainIsotropicLinearHardeningPlasticity",
             hypothesis=mgis_bv.Hypothesis.TRIDIMENSIONAL,
             stabilization_parameter=stabilization_parameter,
@@ -97,86 +97,86 @@ class TestMecha(TestCase):
 
         # plot_det_f(46, "res")
 
-        res_folder = "res"
-        from os import walk, path
-        import matplotlib.pyplot as plt
-        from matplotlib.colors import LinearSegmentedColormap
-
-        def __plot(column: int, time_step_index: int):
-
-            _, _, filenames = next(walk(res_folder))
-            # for time_step_index in range(1, len(time_steps)):
-            # for time_step_index in range(30, len(time_steps)):
-            for filename in filenames:
-                if "{}".format(time_step_index).zfill(6) in filename and "qdp" in filename:
-                    hho_file_path = path.join(res_folder, filename)
-                    with open(hho_file_path, "r") as hho_res_file:
-                        fig, ax0d = plt.subplots(nrows=1, ncols=1)
-                        c_hho = hho_res_file.readlines()
-                        field_label = c_hho[0].split(",")[column]
-                        number_of_points = len(c_hho) - 1
-                        # for _iloc in range(len(c_hho)):
-                        #     line = c_hho[_iloc]
-                        #     x_coordinates = float(line.split(",")[0])
-                        #     y_coordinates = float(line.split(",")[1])
-                        #     if (x_coordinates - 0.0) ** 2 + (y_coordinates)
-                        eucli_d = displacement.euclidean_dimension
-                        points = np.zeros((eucli_d, number_of_points), dtype=real)
-                        field_vals = np.zeros((number_of_points,), dtype=real)
-                        for l_count, line in enumerate(c_hho[1:]):
-                            x_coordinates = float(line.split(",")[0])
-                            y_coordinates = float(line.split(",")[1])
-                            field_value = float(line.split(",")[column])
-                            points[0, l_count] += x_coordinates
-                            points[1, l_count] += y_coordinates
-                            field_vals[l_count] += field_value
-                        x, y = points
-                        colors = [(0, 0, 1), (0, 1, 1), (0, 1, 0), (1, 1, 0), (1, 0, 0)]
-                        perso = LinearSegmentedColormap.from_list("perso", colors, N=1000)
-                        vmin = min(field_vals[:])
-                        vmax = max(field_vals[:])
-                        # vmin = -3.656e9
-                        # vmax = 1.214e9
-                        levels = np.linspace(vmin, vmax, 50, endpoint=True)
-                        ticks = np.linspace(vmin, vmax, 10, endpoint=True)
-                        datad = ax0d.tricontourf(x, y, field_vals[:], cmap=perso, levels=levels)
-                        ax0d.get_xaxis().set_visible(False)
-                        ax0d.get_yaxis().set_visible(False)
-                        ax0d.set_xlabel("map of the domain $\Omega$")
-                        cbar = fig.colorbar(datad, ax=ax0d, ticks=ticks)
-                        cbar.set_label("{} : {}".format(field_label, time_step_index), rotation=270, labelpad=15.0)
-                        # plt.savefig("/home/dsiedel/Projects/pythhon/plots/{}.png".format(time_step))
-                        plt.show()
-
-        for tsindex in [1, 50, 100, 192]:
-            # __plot(15, tsindex)
-            pass
-        # __plot(15, 19)
-        __plot(15, 22)
-        # __plot(3)
-
-        # --- POST PROCESSING
-        # from pp.plot_data import plot_data
-        # mtest_file_path = "mtest/finite_strain_isotropic_linear_hardening.res"
-        # hho_res_dir_path = "../../../res"
-        # number_of_time_steps = len(time_steps)
-        # m_x_inedx = 1
-        # m_y_index = 6
-        # d_x_inedx = 4
-        # d_y_inedx = 9
-        # plot_data(mtest_file_path, hho_res_dir_path, number_of_time_steps, m_x_inedx, m_y_index, d_x_inedx, d_y_inedx)
-        # m_x_inedx = 1
-        # m_y_index = 7
-        # d_x_inedx = 4
-        # d_y_inedx = 10
-        # plot_data(mtest_file_path, hho_res_dir_path, number_of_time_steps, m_x_inedx, m_y_index, d_x_inedx, d_y_inedx)
-        # m_x_inedx = 1
-        # m_y_index = 8
-        # d_x_inedx = 4
-        # d_y_inedx = 11
-        # plot_data(mtest_file_path, hho_res_dir_path, number_of_time_steps, m_x_inedx, m_y_index, d_x_inedx, d_y_inedx)
-        # m_x_inedx = 1
-        # m_y_index = 9
-        # d_x_inedx = 4
-        # d_y_inedx = 12
-        # plot_data(mtest_file_path, hho_res_dir_path, number_of_time_steps, m_x_inedx, m_y_index, d_x_inedx, d_y_inedx)
+        # res_folder = "res"
+        # from os import walk, path
+        # import matplotlib.pyplot as plt
+        # from matplotlib.colors import LinearSegmentedColormap
+        #
+        # def __plot(column: int, time_step_index: int):
+        #
+        #     _, _, filenames = next(walk(res_folder))
+        #     # for time_step_index in range(1, len(time_steps)):
+        #     # for time_step_index in range(30, len(time_steps)):
+        #     for filename in filenames:
+        #         if "{}".format(time_step_index).zfill(6) in filename and "qdp" in filename:
+        #             hho_file_path = path.join(res_folder, filename)
+        #             with open(hho_file_path, "r") as hho_res_file:
+        #                 fig, ax0d = plt.subplots(nrows=1, ncols=1)
+        #                 c_hho = hho_res_file.readlines()
+        #                 field_label = c_hho[0].split(",")[column]
+        #                 number_of_points = len(c_hho) - 1
+        #                 # for _iloc in range(len(c_hho)):
+        #                 #     line = c_hho[_iloc]
+        #                 #     x_coordinates = float(line.split(",")[0])
+        #                 #     y_coordinates = float(line.split(",")[1])
+        #                 #     if (x_coordinates - 0.0) ** 2 + (y_coordinates)
+        #                 eucli_d = displacement.euclidean_dimension
+        #                 points = np.zeros((eucli_d, number_of_points), dtype=real)
+        #                 field_vals = np.zeros((number_of_points,), dtype=real)
+        #                 for l_count, line in enumerate(c_hho[1:]):
+        #                     x_coordinates = float(line.split(",")[0])
+        #                     y_coordinates = float(line.split(",")[1])
+        #                     field_value = float(line.split(",")[column])
+        #                     points[0, l_count] += x_coordinates
+        #                     points[1, l_count] += y_coordinates
+        #                     field_vals[l_count] += field_value
+        #                 x, y = points
+        #                 colors = [(0, 0, 1), (0, 1, 1), (0, 1, 0), (1, 1, 0), (1, 0, 0)]
+        #                 perso = LinearSegmentedColormap.from_list("perso", colors, N=1000)
+        #                 vmin = min(field_vals[:])
+        #                 vmax = max(field_vals[:])
+        #                 # vmin = -3.656e9
+        #                 # vmax = 1.214e9
+        #                 levels = np.linspace(vmin, vmax, 50, endpoint=True)
+        #                 ticks = np.linspace(vmin, vmax, 10, endpoint=True)
+        #                 datad = ax0d.tricontourf(x, y, field_vals[:], cmap=perso, levels=levels)
+        #                 ax0d.get_xaxis().set_visible(False)
+        #                 ax0d.get_yaxis().set_visible(False)
+        #                 ax0d.set_xlabel("map of the domain $\Omega$")
+        #                 cbar = fig.colorbar(datad, ax=ax0d, ticks=ticks)
+        #                 cbar.set_label("{} : {}".format(field_label, time_step_index), rotation=270, labelpad=15.0)
+        #                 # plt.savefig("/home/dsiedel/Projects/pythhon/plots/{}.png".format(time_step))
+        #                 plt.show()
+        #
+        # for tsindex in [1, 50, 100, 192]:
+        #     # __plot(15, tsindex)
+        #     pass
+        # # __plot(15, 19)
+        # __plot(15, 22)
+        # # __plot(3)
+        #
+        # # --- POST PROCESSING
+        # # from pp.plot_data import plot_data
+        # # mtest_file_path = "mtest/finite_strain_isotropic_linear_hardening.res"
+        # # hho_res_dir_path = "../../../res"
+        # # number_of_time_steps = len(time_steps)
+        # # m_x_inedx = 1
+        # # m_y_index = 6
+        # # d_x_inedx = 4
+        # # d_y_inedx = 9
+        # # plot_data(mtest_file_path, hho_res_dir_path, number_of_time_steps, m_x_inedx, m_y_index, d_x_inedx, d_y_inedx)
+        # # m_x_inedx = 1
+        # # m_y_index = 7
+        # # d_x_inedx = 4
+        # # d_y_inedx = 10
+        # # plot_data(mtest_file_path, hho_res_dir_path, number_of_time_steps, m_x_inedx, m_y_index, d_x_inedx, d_y_inedx)
+        # # m_x_inedx = 1
+        # # m_y_index = 8
+        # # d_x_inedx = 4
+        # # d_y_inedx = 11
+        # # plot_data(mtest_file_path, hho_res_dir_path, number_of_time_steps, m_x_inedx, m_y_index, d_x_inedx, d_y_inedx)
+        # # m_x_inedx = 1
+        # # m_y_index = 9
+        # # d_x_inedx = 4
+        # # d_y_inedx = 12
+        # # plot_data(mtest_file_path, hho_res_dir_path, number_of_time_steps, m_x_inedx, m_y_index, d_x_inedx, d_y_inedx)
